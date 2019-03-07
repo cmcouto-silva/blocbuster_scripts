e235431b5b7521311ca0dbce47de4d39e4878355
Cainã Max Couto-Silva <cmcouto.silva@gmail.com>
2019-03-07 10:50:41 -0300

    Update ped_to_blocbuster.sh

--- scripts/ped_to_blocbuster.sh
+++ scripts/ped_to_blocbuster.sh
@@ -6,10 +6,10 @@ out=$2
 cut -d' ' -f7- $plink.ped | sed "s/ //g" | sed "s/.\{2\}/&\t/g" > $out
 
 # Adding column for individuals
-cut -d' ' -f2 $plink.ped > cut_f2_$plink.txt
-paste -d'\t' cut_f2_$plink.txt $out > tmp & mv tmp $out
+cut -d' ' -f2 $plink.ped > $plink_tmp.txt
+paste -d'\t' $plink_tmp.txt $out > tmp & mv tmp $out
 
 # Adding line for SNPs
-echo indiv `cut -f2 $plink.map` > cut_f2_$plink.txt
-cat cut_f2_$plink.txt $out > tmp && mv tmp $out
-rm cut_f2_$plink.txt
+echo indiv `cut -f2 $plink.map` > $plink_tmp.txt
+cat $plink_tmp.txt $out > tmp && mv tmp $out
+rm $plink_tmp.txt
